69597233a4e2e482bf891443c6da0c7156309ea5
najdiazpa-unal <najdiazpa@unal.edu.co>
2025-06-19 21:27:43 -0500

    initial css config and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AsignaturasAdmin from './pages/admin/Asignaturas.jsx';
+import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/admin/asignaturas" element={<AsignaturasAdmin />} />
         <Route path="*" element={<div className="p-4">Página no encontrada</div>} />
       </Routes>
