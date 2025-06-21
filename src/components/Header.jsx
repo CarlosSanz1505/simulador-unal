cbed4ed46c5d61a655ab0d614c1d8efba891aeb1
najdiazpa-unal <najdiazpa@unal.edu.co>
2025-06-21 01:50:51 -0500

    Cards y colores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between px-8 py-4 bg-zinc-50 border-b border-gray-200">
-      <h1 className="text-xl font-bold text-slate-900">Simulador UNAL</h1>
+    <div className="flex items-center justify-between px-8 py-4 bg-white border-b border-gray-200">
+      <h2 className="text-lg text-zinc-500">Mis Simulaciones</h2>
       <button
-        className="px-4 py-2 text-slate-900 bg-zinc-50 rounded hover:bg-red-700 transition"
+        className="px-4 py-2 text-slate-900 border border-slate-900 rounded hover:bg-lime-100 transition"
         onClick={() => alert("Cerrar sesión")}
       >
         Cerrar sesión
