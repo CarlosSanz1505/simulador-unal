41d71cc2dba29a0bf92ba32a95dc1793f46a9ff8
najdiazpa-unal <najdiazpa@unal.edu.co>
2025-06-22 21:29:24 -0500

    Revert "initial css config and styles"

--- src/components/ContadorCreditos.jsx
+++
@@ -1,9 +0,0 @@
-const ContadorCreditos = ({ creditos, tipologia }) => {
-  return (
-    <div className="shadow-lg rounded-lg p-4 mb-6 border border-lime-700 flex flex-col items-center justify-center">
-      <h2 className="text-center font-semibold">{tipologia}</h2>
-      <span className="text-xl font-bold">{creditos}</span>
-    </div>
-  );
-}
-export default ContadorCreditos;
